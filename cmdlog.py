3c7d92a833810ad296c4079c1c18ebaf3fc9ce28
Zedenar <36276054+Zedenar@users.noreply.github.com>
2024-02-21 17:59:10 +0100

    Update cmdlog.py

--- cmdlog.py
+++ cmdlog.py
@@ -1,9 +1,7 @@
 import os
 from settings.config import cmdmenu_path
-from settings.config import ClearCommand
 
 if True:
-    ClearCommand
     os.system(f"python3 {cmdmenu_path}")
 else:
     quit()
