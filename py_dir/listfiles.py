4f791ff3e0cae3428f8cdbf217a12d554cd96097
Zedenar <36276054+Zedenar@users.noreply.github.com>
2024-02-21 18:00:40 +0100

    Update listfiles.py

--- py_dir/listfiles.py
+++ py_dir/listfiles.py
@@ -1,14 +1,22 @@
 import os
 from settings.config import settingslog_path
+from rich.console import Console
+from rich.table import Table
 
-print('-----LOGFILES-----')
+style = ""
+
+console = Console(width=100)
 
 lslog = [file for file in os.listdir(settingslog_path) if file.endswith('.log')]
 file_count = len(lslog)
+table = Table()
+lslog = lslog.__str__()
 
-if file_count > 0:
-    print(lslog)
-else:
-    print("No files found!")
+with console.pager():
+    if file_count > 0:
+        table.add_column("Logfiles", style = "green", justify="center")
+        table.add_row(lslog)
+        console.print(table)
 
-print('------------------')
+    else:
+        console.print("No files found!", style=style, justify="right")
